38af25249f7b10e3cd1c6be971f2ca8c7bab0170
ChoYoungshin <cyshin1031@naver.com>
2025-04-09 19:25:05 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd2
+          Learn GithubAction cidcd test
         </a>
       </header>
     </div>
